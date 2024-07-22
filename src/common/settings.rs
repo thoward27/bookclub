18edf23b5fbb465e42654ca33c960224e7a21929
Tom <info@tomhoward.codes>
2024-07-22 08:14:53 -0400

    chore(settings): implement defaults

--- src/common/settings.rs
+++ src/common/settings.rs
@@ -2,7 +2,7 @@ use loco_rs::prelude::*;
 use serde::{Deserialize, Serialize};
 
 // put this in src/common/settings.rs
-#[derive(Serialize, Deserialize, Default, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Settings {
     pub title: String,
 }
@@ -10,5 +10,13 @@ pub struct Settings {
 impl Settings {
     pub fn from_json(value: &serde_json::Value) -> Result<Self, Error> {
         Ok(serde_json::from_value(value.clone())?)
+    }
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            title: "Bookclub".to_string(),
+        }
     }
 }
