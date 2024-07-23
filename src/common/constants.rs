430e7d619af73ac879b16d0a4e0e2396b5675e53
Tom <info@tomhoward.codes>
2024-07-23 06:41:54 -0400

    feat: add env-var constants

---
+++ src/common/constants.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::fmt;
+use std::fmt::Display;
+use std::sync::LazyLock;
+
+pub const DEFAULT_TITLE: &str = "Inkwell Collective";
+
+#[derive(Debug)]
+pub struct Constant<T> {
+    pub value: LazyLock<T>,
+}
+
+impl<T: std::fmt::Display> Display for Constant<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", *self.value)
+    }
+}
+
+pub static TITLE: Constant<String> = Constant {
+    value: LazyLock::new(|| env::var("TITLE").unwrap_or_else(|_| DEFAULT_TITLE.to_string())),
+};
