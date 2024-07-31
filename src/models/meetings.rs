930d1fa9064df47400a68db82c15defb2a25a12d
Tom <info@tomhoward.codes>
2024-07-31 08:55:49 -0400

    feat: meetings page with editable locations

--- src/models/meetings.rs
+++ src/models/meetings.rs
@@ -1,6 +1,18 @@
-use super::_entities::meetings::ActiveModel;
-use sea_orm::entity::prelude::*;
+use super::_entities::meetings::{ActiveModel, Model};
+use loco_rs::prelude::*;
+use sea_orm::ActiveValue;
 
 impl ActiveModelBehavior for ActiveModel {
     // extend activemodel below (keep comment for generators)
+}
+
+impl super::_entities::meetings::ActiveModel {
+    pub async fn set_location(
+        mut self,
+        location: String,
+        db: &DatabaseConnection,
+    ) -> ModelResult<Model> {
+        self.location = ActiveValue::set(location);
+        Ok(self.update(db).await?)
+    }
 }
