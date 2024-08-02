a3d91a69b9c3e7a1f6c05fe10e188c70179c2980
Tom <info@tomhoward.codes>
2024-08-02 10:56:13 -0400

    feat: change order for future meetings before they are created

--- src/models/meetings.rs
+++ src/models/meetings.rs
@@ -1,12 +1,73 @@
 use super::_entities::meetings::{ActiveModel, Model};
+use super::_entities::users;
+use futures::{stream, StreamExt};
 use loco_rs::prelude::*;
 use sea_orm::ActiveValue;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug)]
+pub struct MeetingUpdateParams {
+    pub location: String,
+    pub date: chrono::DateTime<chrono::FixedOffset>,
+    pub order: Vec<i32>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct NextMeetingTemplate {
+    pub order: Vec<users::Model>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct NextMeetingTemplateRaw {
+    order: Vec<i32>,
+}
+
+impl super::_entities::meetings::Model {
+    pub async fn get_next_meeting_template(
+        &self,
+        db: &DatabaseConnection,
+    ) -> Result<NextMeetingTemplate> {
+        let template: NextMeetingTemplateRaw =
+            serde_json::from_value(self.next_meeting_template.clone()).unwrap();
+        Ok(NextMeetingTemplate {
+            order: stream::iter(template.order)
+                .then(|user_id| async move {
+                    users::Entity::find()
+                        .filter(users::Column::Id.eq(user_id))
+                        .one(db)
+                        .await
+                        .unwrap()
+                        .unwrap()
+                })
+                .collect::<Vec<users::Model>>()
+                .await,
+        })
+        // Ok(serde_json::from_value(self.next_meeting_template.clone()).unwrap())
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {
     // extend activemodel below (keep comment for generators)
 }
 
 impl super::_entities::meetings::ActiveModel {
+    pub async fn update_from_params(
+        mut self,
+        params: MeetingUpdateParams,
+        db: &DatabaseConnection,
+    ) -> ModelResult<Model> {
+        self.location = ActiveValue::set(params.location);
+        self.date = ActiveValue::set(params.date);
+        if self.next_meeting_id.is_not_set() {
+            self.next_meeting_template = ActiveValue::set(
+                serde_json::to_value(NextMeetingTemplateRaw {
+                    order: params.order,
+                })
+                .unwrap(),
+            );
+        }
+        Ok(self.update(db).await?)
+    }
     pub async fn set_location(
         mut self,
         location: String,
--- src/views/meetings.rs
+++ src/views/meetings.rs
@@ -1,4 +1,5 @@
-use crate::models::_entities::{books, meetings};
+use crate::models::_entities::{books, meetings, users};
+use crate::models::meetings::NextMeetingTemplate;
 use askama_axum::Template;
 
 use loco_rs::prelude::*;
@@ -9,7 +10,30 @@ use std::vec::Vec;
 #[template(path = "components/meeting_detail.html")]
 pub struct MeetingDetail {
     pub meeting: meetings::Model,
+    pub book: books::Model,
+    pub user: users::Model,
+
+    pub next_meeting_template: NextMeetingTemplate,
     pub editor: bool,
+}
+
+impl MeetingDetail {
+    pub async fn new(
+        meeting: meetings::Model,
+        book: books::Model,
+        user: users::Model,
+        db: &DatabaseConnection,
+    ) -> Self {
+        let next_meeting_template = meeting.get_next_meeting_template(db).await.unwrap();
+        let editor = user.id == book.user_id;
+        Self {
+            meeting,
+            book,
+            user,
+            next_meeting_template,
+            editor,
+        }
+    }
 }
 
 impl ViewRenderer for MeetingDetail {
